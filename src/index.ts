27d9d3fa607cad05cbcc9d1b8ca0f35153c122fe
Italo Bezerra <contato.italocarlosbezerra@gmail.com>
2022-11-30 18:57:46 -0300

    routes finished

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import usersRoute from "./routes/users.route";
 
 const app = express();
 
+// Configuração da aplicação
+app.use(express.json());
+
+// Configuração de rotas
 app.use(usersRoute);
 
+// Inicialização do servidor
 app.listen(3000, () => {
   console.log("Servidor conectado e rodando na porta 3000!");
 });
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,14 +1,30 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const usersRoute = Router();
 
 usersRoute.get("/users", (req: Request, res: Response, next: NextFunction) => {
   const users = [{ userName: "Italo" }];
-  res.status(200).json(users);
+  res.status(StatusCodes.OK).json(users);
 });
 
-usersRoute.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.status(200).send({ message: "Tudo Ok!" });
+usersRoute.get("/users/:uuid", (req: Request, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(StatusCodes.OK).json({ message: `ID buscado foi: ${uuid}` });
+});
+
+usersRoute.post("/users", (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+  res.status(StatusCodes.CREATED).send(newUser);
+});
+
+usersRoute.put("/users/:uuid", (req: Request, res: Response, next: NextFunction) => {
+  const uuid = req.params.uuid;
+  res.status(StatusCodes.OK).send({ uuid });
+});
+
+usersRoute.delete("/users/:uuid", (req: Request, res: Response, next: NextFunction) => {
+  res.sendStatus(StatusCodes.OK);
 });
 
 export default usersRoute;
